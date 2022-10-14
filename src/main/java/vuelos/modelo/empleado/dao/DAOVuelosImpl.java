2bdf9f6060b61cfa241302f721f70818ec68ebf7
EzePerez <ezequielalejandroperez@hotmail.com>
2022-10-14 09:36:33 -0300

    modifcacion recuperarVuelosDisponibles y recuperarDetalleVuelo

--- src/main/java/vuelos/modelo/empleado/dao/DAOVuelosImpl.java
+++ src/main/java/vuelos/modelo/empleado/dao/DAOVuelosImpl.java
@@ -1,6 +1,9 @@
 package vuelos.modelo.empleado.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -37,11 +40,55 @@ public class DAOVuelosImpl implements DAOVuelos {
 		 *      Nota: para acceder a la B.D. utilice la propiedad "conexion" que ya tiene una conexión
 		 *      establecida con el servidor de B.D. (inicializada en el constructor DAOVuelosImpl(...)).  
 		 */
-		//Datos estáticos de prueba. Quitar y reemplazar por código que recupera los datos reales.
+		/**Datos estáticos de prueba. Quitar y reemplazar por código que recupera los datos reales.
 		ArrayList<InstanciaVueloBean> resultado = DAOVuelosDatosPrueba.generarVuelos(fechaVuelo);  
 		
 		return resultado;
-		// Fin datos estáticos de prueba.
+		Fin datos estáticos de prueba.
+		*/
+		logger.info("Lista de vuelos de la fecha {}", vuelos.utils.Fechas.convertirDateAString(fechaVuelo));
+		ArrayList<InstanciaVueloBean> resultado = new ArrayList<InstanciaVueloBean>();
+		
+		String sql = "SELECT DISTINCT nro_vuelo, modelo, fecha, dia_sale, hora_sale, hora_llega, tiempo_estimado, codigo_aero_sale, nombre_aero_sale, ciudad_sale, codigo_aero_llega, nombre_aero_llega, ciudad_llega"+
+					" FROM vuelos_disponibles WHERE fecha='"+vuelos.utils.Fechas.convertirDateADateSQL(fechaVuelo)+
+					"' and ciudad_sale='"+origen.getCiudad()+"' and ciudad_llega='"+destino.getCiudad()+"';";
+		try {
+			PreparedStatement stmt = conexion.prepareStatement(sql);
+			ResultSet rs= stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				logger.info("Lista de vuelos de la fecha {}", vuelos.utils.Fechas.convertirDateAString(fechaVuelo));
+				InstanciaVueloBean vuelo = new InstanciaVueloBeanImpl();
+				vuelo.setNroVuelo(rs.getString("nro_vuelo"));
+				vuelo.setModelo(rs.getString("modelo"));
+				vuelo.setDiaSalida(rs.getString("dia_sale"));
+				vuelo.setHoraSalida(rs.getTime("hora_sale"));
+				vuelo.setHoraLlegada(rs.getTime("hora_llega"));
+				vuelo.setTiempoEstimado(rs.getTime("tiempo_estimado"));
+				vuelo.setFechaVuelo(vuelos.utils.Fechas.convertirStringADateSQL(rs.getString("fecha")));
+				
+				AeropuertoBean aeroSale = new AeropuertoBeanImpl();
+				aeroSale.setCodigo(rs.getString("codigo_aero_sale"));
+				aeroSale.setNombre(rs.getString("nombre_aero_sale"));
+				aeroSale.setUbicacion(origen);
+				vuelo.setAeropuertoSalida(aeroSale);
+				
+				AeropuertoBean aeroLlega = new AeropuertoBeanImpl();
+				aeroLlega.setCodigo(rs.getString("codigo_aero_llega"));
+				aeroLlega.setNombre(rs.getString("nombre_aero_llega"));
+				aeroLlega.setUbicacion(destino);
+				vuelo.setAeropuertoLlegada(aeroLlega);
+				
+				resultado.add(vuelo);
+			}
+		}catch (SQLException ex) {
+			logger.error("SQLException: " + ex.getMessage());
+			logger.error("SQLState: " + ex.getSQLState());
+			logger.error("VendorError: " + ex.getErrorCode());		   
+			throw new Exception("Error en la conexión con la BD.");
+	   }
+		return resultado;
+		
 	}
 
 	@Override
@@ -53,10 +100,29 @@ public class DAOVuelosImpl implements DAOVuelos {
 		 *      Nota: para acceder a la B.D. utilice la propiedad "conexion" que ya tiene una conexión
 		 *      establecida con el servidor de B.D. (inicializada en el constructor DAOVuelosImpl(...)).
 		 */
-		//Datos estáticos de prueba. Quitar y reemplazar por código que recupera los datos reales.
-		ArrayList<DetalleVueloBean> resultado = DAOVuelosDatosPrueba.generarDetalles(vuelo);
 		
+		ArrayList<DetalleVueloBean> resultado = new ArrayList<DetalleVueloBean>();
+		
+		String sql = "SELECT clase, precio, asientos_disponibles FROM vuelos_disponibles WHERE fecha='"+vuelos.utils.Fechas.convertirDateADateSQL(vuelo.getFechaVuelo())+"' and nro_vuelo='"+vuelo.getNroVuelo()+"';";
+		
+		try {
+			PreparedStatement stmt = conexion.prepareStatement(sql);
+			ResultSet rs= stmt.executeQuery(sql);
+			while(rs.next()) {
+				DetalleVueloBean dvuelo = new DetalleVueloBeanImpl();
+				dvuelo.setAsientosDisponibles(rs.getInt("asientos_disponibles"));
+				dvuelo.setClase(rs.getString("clase"));
+				dvuelo.setPrecio(rs.getInt("precio"));
+				dvuelo.setVuelo(vuelo);
+				
+				resultado.add(dvuelo);
+			}
+		}catch (SQLException ex) {
+			logger.error("SQLException: " + ex.getMessage());
+			logger.error("SQLState: " + ex.getSQLState());
+			logger.error("VendorError: " + ex.getErrorCode());		   
+			throw new Exception("Error en la conexión con la BD.");
+	   }
 		return resultado; 
-		// Fin datos estáticos de prueba.
 	}
 }
